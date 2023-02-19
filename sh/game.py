5e3f225c5fe26f12bd81cfb69481f7b932ed1ed9
niezwyklyable <niezwyklyable@gmail.com>
2023-02-19 16:42:52 +0100

    Developed sliding background
     - created two identical backgrounds shifted in dim x which slide smoothly and alternately without white gaps
    
    	modified:   sh/game.py

--- sh/game.py
+++ sh/game.py
@@ -1,5 +1,5 @@
 import pygame
-from .constants import WHITE, BACKGROUND
+from .constants import WHITE, BACKGROUND, WIDTH
 from .player import Player
 
 class Game():
@@ -7,11 +7,20 @@ class Game():
         self.win = win
         self.player = None
         self.create_player()
+        self.bg1_x = 0
+        self.bg2_x = BACKGROUND.get_width()
         
     def render(self):
         # background
         self.win.fill(WHITE)
-        self.win.blit(BACKGROUND, (0, 0))
+        self.bg1_x -= 1
+        self.bg2_x -= 1
+        self.win.blit(BACKGROUND, (self.bg1_x, 0)) # first background
+        self.win.blit(BACKGROUND, (self.bg2_x, 0)) # second background
+        if self.bg1_x <= -2*BACKGROUND.get_width() + WIDTH:
+            self.bg1_x = WIDTH
+        if self.bg2_x <= -BACKGROUND.get_width():
+            self.bg2_x = BACKGROUND.get_width()
 
         # player
         if self.player:
